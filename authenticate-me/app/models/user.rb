5b175d8dccb01de1293ea468ba9dc3d87335db26
lewiskk016 <lewiskk016@gmail.com>
2023-05-23 14:56:02 -0400

    finish phase 2

--- authenticate-me/app/models/user.rb
+++ authenticate-me/app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   # validates :password, length: {minimum: 6}, allow_nil: true
   # validates :email, presence: true, uniqueness: true
 
-  validates :username, 
-  uniqueness: true, 
-  length: { in: 3..30 }, 
+  validates :username,
+  uniqueness: true,
+  length: { in: 3..30 },
   format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
-validates :email, 
-  uniqueness: true, 
-  length: { in: 3..255 }, 
+validates :email,
+  uniqueness: true,
+  length: { in: 3..255 },
   format: { with: URI::MailTo::EMAIL_REGEXP }
 validates :session_token, presence: true, uniqueness: true
 validates :password, length: { in: 6..255 }, allow_nil: true
@@ -40,3 +40,46 @@ def ensure_session_token
 end
 
 end
+
+
+
+# class User < ApplicationRecord
+#   has_secure_password
+
+#   validates :username,
+#     uniqueness: true,
+#     length: { in: 3..30 },
+#     format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+#   validates :email,
+#     uniqueness: true,
+#     length: { in: 3..255 },
+#     format: { with: URI::MailTo::EMAIL_REGEXP }
+#   validates :session_token, presence: true, uniqueness: true
+#   validates :password, length: { in: 6..255 }, allow_nil: true
+
+#   before_validation :ensure_session_token
+
+#   def self.find_by_credentials(credential, password)
+#     field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :username
+#     user = User.find_by(field => credential)
+#     user&.authenticate(password)
+#   end
+
+#   def reset_session_token!
+#     self.update!(session_token: generate_unique_session_token)
+#     self.session_token
+#   end
+
+#   private
+
+#   def generate_unique_session_token
+#     loop do
+#       token = SecureRandom.base64
+#       break token unless User.exists?(session_token: token)
+#     end
+#   end
+
+#   def ensure_session_token
+#     self.session_token ||= generate_unique_session_token
+#   end
+# end
